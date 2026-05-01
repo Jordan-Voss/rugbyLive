f18092f6fbe7813d7afa0f2f4fa5df0dd78a9a03
Jordan-Voss <jordanvoss98@gmail.com>
2026-05-01 10:07:30 +0100

    add more resources to main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,11 +21,115 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_security_group" "api_sg" {
+  name        = "rugbylive-api-sg"
+  description = "Allow SSH, HTTP and HTTPS traffic"
+
+  ingress {
+    description = "SSH from GitHub Actions and local machine"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "rugbylive-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "rugbylive-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_key_pair" "deploy" {
+  key_name   = "rugbylive-key"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "api" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.deploy.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  vpc_security_group_ids = [aws_security_group.api_sg.id]
+
+  tags = {
+    Name    = "RugbyLive-API"
+    Project = "RugbyLive"
+  }
+}
+
 resource "aws_eip" "api_ip" {
   instance = aws_instance.api.id
   domain   = "vpc"
+
+  tags = {
+    Name    = "RugbyLive-API-IP"
+    Project = "RugbyLive"
+  }
 }
 
 resource "aws_ecr_repository" "api" {
-  name = "rugbylive-api"
+  name                 = "rugbylive-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Project = "RugbyLive"
+  }
 }
