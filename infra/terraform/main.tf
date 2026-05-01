c9bf91d0ec59bbf0184e76840f60ac62ca54f64c
Jordan-Voss <jordanvoss98@gmail.com>
2026-05-01 09:30:55 +0100

    add bucket to save terraform state

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,94 +1,22 @@
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "rugbylive-terraform-state"
+    key          = "api/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-}
-
-resource "aws_security_group" "api_sg" {
-  name = "rugbylive-api-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # allow GH actions
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_iam_role" "ec2_role" {
-  name = "rugbylive-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_read" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "rugbylive-ec2-profile"
-  role = aws_iam_role.ec2_role.name
-}
-
-resource "aws_key_pair" "deploy" {
-  key_name   = "rugbylive-key"
-  public_key = file(var.public_key_path)
-}
-
-resource "aws_instance" "api" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = aws_key_pair.deploy.key_name
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.api_sg.id]
-
-  tags = {
-    Name = "RugbyLive-API"
-  }
-}
-
-resource "aws_eip" "api_ip" {
-  instance = aws_instance.api.id
-  domain   = "vpc"
-}
-
-resource "aws_ecr_repository" "api" {
-  name = "rugbylive-api"
 }
