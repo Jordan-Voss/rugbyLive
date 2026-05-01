f25b17c9913c7a3f921b62f63ead0d282232318b
Jordan-Voss <jordanvoss98@gmail.com>
2026-05-01 09:22:50 +0100

    update terraform method to set up ec2 from one main file and install docker and aws

--- infra/terraform/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "api" {
-  name                 = "${var.project_name}-api"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,25 +1,3 @@
-variable "aws_region" {
-  type    = string
-  default = "eu-west-1"
-}
-
-variable "project_name" {
-  type    = string
-  default = "rugbylive"
-}
-
-variable "ssh_allowed_cidr" {
-  type        = string
-  description = "Your IP in CIDR format, e.g. 1.2.3.4/32"
-}
-
 variable "public_key_path" {
-  type        = string
-  description = "Path to your local SSH public key"
-  default     = "~/.ssh/rugbylive.pub"
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"
+  type = string
 }
